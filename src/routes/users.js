1942e0cef143da3482e0e4cfc598776c97215651
zhangzeyu01 <zhangzeyu01@baidu.com>
2022-03-08 19:54:33 +0800

    update

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,12 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.get('/:username', function (ctx, next) {
+  const { username } = ctx.params
+  ctx.body = {
+    title: 'haha',
+    username
+  }
+})
+
 module.exports = router
