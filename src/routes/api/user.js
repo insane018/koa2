e304e320952936cfaba8619a369856d87a7230e8
zhangzeyu01 <zhangzeyu01@baidu.com>
2022-04-25 20:02:10 +0800

    Revert "4"
    This reverts commit b6548c5df03167f0d2a2f25dba3a0234aa2fea31.

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -7,7 +7,7 @@ router.prefix('/api/user')
 
 
 
-router.post('/registerddd', genValidator(validateUser), async (ctx, next) => {
+router.post('/register', genValidator(validateUser), async (ctx, next) => {
     const {userName, password, gender} = ctx.request.body
     
     ctx.body = await register({
